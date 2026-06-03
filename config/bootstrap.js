ab47a380d8c689e769b8b8ab24d1de7aa671b28c
juan.lema14 <juan.lema1408@gmail.com>
2026-06-02 23:13:22 -0500

    probando sin imagenes

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,18 +1,6 @@
-/**
- * Seed Function
- * (sails.config.bootstrap)
- *
- * A function that runs just before your Sails app gets lifted.
- * > Need more flexibility?  You can also create a hook.
- *
- * For more information on seeding your app with fake data, check out:
- * https://sailsjs.com/config/bootstrap
- */
-
 module.exports.bootstrap = async function (done) {
   try {
     await sails.helpers.cargarExcel.with({ accion: 'cargar' })
-    await sails.helpers.mycloudImages.with({ accion: 'cargar' })
     return done()
   } catch (error) {
     sails.log.error('Error en bootstrap:', error)
